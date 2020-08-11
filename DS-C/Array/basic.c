79d1b3fd2adc6f0eb1290aa547b1e8bdab9fe262
aka-dmv <aka.dmv@gmail.com>
2020-08-11 16:04:12 -0700

    Linear and Binary Search code

--- DS-C/Array/basic.c
+++ DS-C/Array/basic.c
@@ -3,7 +3,7 @@
 
 struct newArray
 {
-    int A[10];
+    int A[15];
     int size;
     int length;
 };
@@ -56,14 +56,58 @@ int Delete(struct newArray *arr, int index)
         }
     }
 }
+int LinearSearch(struct newArray *arr, int key)
+{
+
+    for (int i = 0; i < arr->length; i++)
+    {
+        if (arr->A[i] == key)
+        {
+            int temp = arr->A[0];
+            arr->A[0] = key;
+            arr->A[i] = temp;
+
+            return i;
+        }
+    }
+    return 000;
+}
+
+int BinarySearch(struct newArray *arr, int key)
+{
+    int mid;
+    int h = arr->length - 1;
+    int l = 0;
+    while (l <= h)
+    {
+        mid = (l + h) / 2;
+        if (arr->A[mid] == key)
+        {
+            return mid;
+        }
+        else if (arr->A[mid] > key)
+        {
+            h = mid - 1;
+        }
+        else
+        {
+            l = mid + 1;
+        }
+    }
+    return -1;
+}
 int main()
 {
     int i, n;
-    struct newArray arr = {{10, 20, 30, 40, 50}, 10, 5};
+    struct newArray arr = {{10, 20, 30, 40, 50, 60, 70, 80, 90}, 15, 9};
     // Append(&arr, 60);
     // Insert(&arr, 1, 33);
-    int delValue = Delete(&arr, 4);
+    // int delValue = Delete(&arr, 4);
+    // printf("\nDeleted value is %d", delValue);
+    // int position = LinearSearch(&arr, 50);
+    // printf("\n\nElement found at position %d", position);
+    int keyIndex = BinarySearch(&arr, 30);
+    printf("\nKey found at array index %d", keyIndex);
     Display(arr);
-    printf("\nDeleted value is %d", delValue);
-    printf("\nLength of append array is %d", arr.length);
+    // printf("\nLength of append array is %d", arr.length);
 }
